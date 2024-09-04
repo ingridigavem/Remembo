0481efbdfc6a16f3d70f1600e393935623958e67
Ingridi Gave <ingridigavem@gmail.com>
2024-09-03 22:42:09 -0300

    Adds form to create review

--- frontend/src/components/home-panel/dashboard/stats.tsx
+++ frontend/src/components/home-panel/dashboard/stats.tsx
@@ -8,18 +8,6 @@ export function Stats() {
                     <CardTitle className="text-sm font-medium">
                         Total de reviões concluídas
                     </CardTitle>
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        className="h-4 w-4 text-muted-foreground"
-                    >
-                        <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
-                    </svg>
                 </CardHeader>
                 <CardContent>
                     <div className="text-2xl font-bold">20</div>
@@ -30,18 +18,6 @@ export function Stats() {
                     <CardTitle className="text-sm font-medium">
                         Total de conteúdos finalizados
                     </CardTitle>
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        className="h-4 w-4 text-muted-foreground"
-                    >
-                        <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
-                    </svg>
                 </CardHeader>
                 <CardContent>
                     <div className="text-2xl font-bold">5</div>
@@ -52,18 +28,6 @@ export function Stats() {
                     <CardTitle className="text-sm font-medium">
                         Total de conteúdos em aberto
                     </CardTitle>
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        className="h-4 w-4 text-muted-foreground"
-                    >
-                        <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
-                    </svg>
                 </CardHeader>
                 <CardContent>
                     <div className="text-2xl font-bold">10</div>
--- frontend/src/pages/home/layout.tsx
+++ frontend/src/pages/home/layout.tsx
@@ -1,13 +1,15 @@
+import { CreateReview } from "@/components/home-panel/dashboard/create-review";
 import { Navbar } from "@/components/home-panel/navbar";
 import { Outlet } from "react-router-dom";
 
 export function LayoutHome() {
     return (
-        <div className="min-h-full">
+        <div className="min-h-full relative">
+            <CreateReview />
             <Navbar />
             <div className="lg:py-10">
                 <main>
-                    <div className="mx-auto max-w-7xl px-8 py-8 sm:px-6 lg:px-8">
+                    <div className="relative mx-auto max-w-7xl px-8 py-8 sm:px-6 lg:px-8">
                         <Outlet />
                     </div>
                 </main>
